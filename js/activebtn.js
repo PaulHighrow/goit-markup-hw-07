cb0cf50505f5bdf8218eb821895c59113dc7ce07
Kyrylo Sulihan <paulhighrow@gmail.com>
2022-08-08 03:39:09 +0300

    Версія на перевірку 1.0
    Виконано рефакторинг коду за методологією BEM та препроцессору SASS.

--- js/activebtn.js
+++ js/activebtn.js
@@ -8,14 +8,14 @@
   var btnContainer = document.querySelector('.filter');
 
   // Все элементы фильтра с указанным селектором 
-  var btns = btnContainer.querySelectorAll('.btn');
+  var btns = btnContainer.querySelectorAll('.filter__btn');
 
   // Добавление класса в активную кнопку и удаление из неактивной
   for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', function () {
-      var now = document.getElementsByClassName('active');
-      now[0].className = now[0].className.replace(' active', '');
-      this.className += ' active';
+      var now = document.getElementsByClassName('filter__btn--active');
+      now[0].className = now[0].className.replace(' filter__btn--active', '');
+      this.className += ' filter__btn--active';
     });
   }
 }
--- js/modal.js
+++ js/modal.js
@@ -12,12 +12,12 @@
   // Добавлена возможность закрытия модального окна по нажатию кнопки Escape
   document.addEventListener('keydown', function (event) {
     if (event.key === 'Escape') {
-      refs.modal.classList.add('is-hidden'), refs.body.classList.remove('no-scroll');
+      refs.modal.classList.add('backdrop--is-hidden'), refs.body.classList.remove('no-scroll');
     }
   });
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('backdrop--is-hidden');
     // Отключение скролла при вызове модальной формы
     refs.body.classList.toggle('no-scroll');
   }
